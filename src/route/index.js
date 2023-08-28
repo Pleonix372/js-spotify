a925ae6039d480f33c8a311e58ca23dca49526c2
Pleonix372 <sasha.miroshnikov372@gmail.com>
2023-08-28 15:01:12 +0300

    created js-crud product

--- src/route/index.js
+++ src/route/index.js
@@ -60,6 +60,90 @@ class User {
   }
 }
 
+class Product {
+  static #list = []
+
+  constructor(name, price, description) {
+    this.name = name
+    this.price = price
+    this.description = description
+    this.id = Math.round(Math.random() * 100000)
+    this.createDate = () => {
+      this.date = new Date().toISOString()
+    }
+  }
+
+  static getList = () => {
+    return this.#list
+  }
+  checkId = (id) => this.id === id
+
+  static add = (product) => {
+    this.#list.push(product)
+  }
+
+  static getById = (id) => {
+    this.#list.find((product) => product.id === id)
+  }
+
+  // static updateById = (id, data) => {
+  //   const product = this.getById(id)
+
+  // if (product) {
+  //   this.update(product, data)
+
+  //   return true
+  // } else {
+  //   return false
+  // }
+  // }
+
+  // static update = (
+  //   product,
+  //   { name, price, description },
+  // ) => {
+  //   if (name) {
+  //     product.name = name
+  //   }
+  //   if (price) {
+  //     product.price = price
+  //   }
+  //   if (description) {
+  //     product.description = description
+  //   }
+  // }
+  static updateById = (id, data) => {
+    const product = this.getById(id)
+    const { name } = data
+    if (product) {
+      if (name) {
+        product.name = name
+      }
+      return true
+    } else {
+      return false
+    }
+  }
+
+  static update = (name, { product }) => {
+    if (name) {
+      product.name = name
+    }
+  }
+
+  static deleteById = (id) => {
+    const index = this.#list.findIndex(
+      (product) => product.id === id,
+    )
+    if (index !== -1) {
+      this.#list.splice(index, 1)
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
@@ -135,6 +219,130 @@ router.post('/user-update', function (req, res) {
       ? 'Емайл пошта оновлена'
       : 'Сталася помилка',
   })
+})
+
+// ================================================================
+
+router.get('/product-create', function (req, res) {
+  res.render('product-create', {
+    style: 'product-create',
+  })
+})
+
+// ================================================================
+
+router.post('/product-create', function (req, res) {
+  const { name, price, description } = req.body
+
+  const product = new Product(name, price, description)
+
+  Product.add(product)
+
+  console.log(Product.getList())
+
+  res.render('alert', {
+    style: 'alert',
+    info: 'Товар успішно створено',
+  })
+})
+
+// ================================================================
+
+router.get('/product-list', function (req, res) {
+  const list = Product.getList()
+
+  console.log(list)
+
+  res.render('product-list', {
+    style: 'product-list',
+    data: {
+      products: {
+        list,
+        isEmpty: list.length === 0,
+      },
+    },
+  })
+})
+
+// ================================================================
+
+router.get('/product-edit', function (req, res) {
+  const { id } = req.query
+
+  const product = Product.getById(Number(id))
+
+  console.log(product)
+
+  if (product) {
+    return res.render('product-edit', {
+      style: 'product-edit',
+      data: {
+        name: product.name,
+        price: product.price,
+        id: product.id,
+        description: product.description,
+      },
+    })
+  } else {
+    return res.render('alert', {
+      style: 'alert',
+      info: 'Товар з таким ID не знайдено',
+    })
+  }
+})
+
+// ================================================================
+
+router.post('/product-edit', function (req, res) {
+  const { name, price, description } = req.body
+
+  const product = Product.updateById(Number(id), {
+    name,
+    price,
+    description,
+  })
+  console.log(id)
+  console.log(product)
+  if (product) {
+    res.render('alert', {
+      style: 'alert',
+      info: 'Інформацію про товар оновлено',
+    })
+  } else {
+    res.render('alert', {
+      style: 'alert',
+      info: 'Сталася помилка',
+    })
+  }
+
+  res.render('alert', {
+    style: 'alert',
+    info: result
+      ? 'Виконано'
+      : 'Товар з таким ID не знайдено',
+  })
+})
+
+// ================================================================
+
+router.get('/product-delete', function (req, res) {
+  const { id } = req.query
+
+  const product = Product.deleteById(Number(id))
+
+  console.log(product)
+
+  if (product) {
+    res.render('alert', {
+      style: 'alert',
+      info: 'Товар видалено',
+    })
+  } else {
+    res.render('alert', {
+      style: 'alert',
+      info: 'Сталася помилка',
+    })
+  }
 })
 
 // Підключаємо роутер до бек-енду
